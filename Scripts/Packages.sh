1d19e8c518a7534df398fd5bcb6f7937a64f8d85
迷人的卡勒 <isbtsuns@icloud.com>
2026-03-11 21:36:58 +0800

    Add design theme package to Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -50,7 +50,7 @@ UPDATE_PACKAGE "aurora" "eamonxg/luci-theme-aurora" "master"
 UPDATE_PACKAGE "aurora-config" "eamonxg/luci-app-aurora-config" "master"
 UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "master"
 UPDATE_PACKAGE "kucat-config" "sirpdboy/luci-app-kucat-config" "master"
-
+UPDATE_PACKAGE "design" "papagaye744/luci-theme-design" "js"
 UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
 UPDATE_PACKAGE "momo" "nikkinikki-org/OpenWrt-momo" "main"
 UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
